d126333f0cc47eb882a7f7569acb91939060ed8b
zeny11-11 <zeny430@gmail.com>
2020-11-11 14:19:21 -0800

    Update jquery.chocolat.js

--- js/jquery.chocolat.js
+++ js/jquery.chocolat.js
@@ -716,6 +716,9 @@
         timer             : false,
         timerDebounce     : false,
         images            : [
+                            {src:'http://www.jacqueline-morales.com/images/gallery-images/apch.org.png', title: 'Web Development for apch.org'},
+                            {src:'http://www.jacqueline-morales.com/images/gallery-images/miraclesforkids.org.png', title: 'Web Development for miraclesforkids.org'},
+            
                             {src:'http://www.jacqueline-morales.com/images/gallery-images/shiftcare.jpg', title: 'Web Design for Shiftcare.com'},
                             {src:'http://www.jacqueline-morales.com/images/gallery-images/Drehm.jpg', title: 'Web Design for pharma-strategies.eu'},
                             {src:'http://www.jacqueline-morales.com/images/gallery-images/falcon.jpg', title: 'Brochure Design for Falcon Construction'},
